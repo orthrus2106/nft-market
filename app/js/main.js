8e2a2f23038d5420008835f53db5321d40b4708c
orthrus2106 <130089982+orthrus2106@users.noreply.github.com>
2024-12-31 05:35:06 +0200

    Finished main page, finished registration and profile

--- app/js/main.js
+++ app/js/main.js
@@ -7,3 +7,22 @@ document.querySelector('.burger').addEventListener('click', function () {
     // Переключаем класс 'open' у меню
     nav.classList.toggle('open');
 });
+
+const tabs = document.querySelectorAll('.profile__tabs-item');
+const tabsWrapper = document.querySelector('.profile__tabs');
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener('click', () => {
+    // Убираем активный класс у всех табов
+    tabs.forEach((t) => t.classList.remove('profile__tabs-item--active'));
+
+    // Добавляем активный класс текущему табу
+    tab.classList.add('profile__tabs-item--active');
+
+    // Передвигаем бордер к активному табу
+    tabsWrapper.style.setProperty('--active-tab-index', index);
+  });
+});
+
+window.addEventListener('resize', toggleFormModifier);
+window.addEventListener('load', toggleFormModifier);
