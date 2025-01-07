e27dfa886aeacc26f451aff2a28cb9754ba53aee
orthrus2106 <130089982+orthrus2106@users.noreply.github.com>
2025-01-07 20:59:45 +0200

    Every page is finished

--- app/js/main.js
+++ app/js/main.js
@@ -1,28 +1,63 @@
 document.querySelector('.burger').addEventListener('click', function () {
-    const nav = document.querySelector('.nav');
+  const nav = document.querySelector('.nav');
+  const body = document.body;
 
-    // Переключаем класс 'active' у бургер-меню
-    this.classList.toggle('active');
+  this.classList.toggle('active');
+  nav.classList.toggle('open');
 
-    // Переключаем класс 'open' у меню
-    nav.classList.toggle('open');
+  if (nav.classList.contains('open')) {
+      body.classList.add('no-scroll');
+  } else {
+      body.classList.remove('no-scroll');
+  }
 });
 
-const tabs = document.querySelectorAll('.profile__tabs-item');
-const tabsWrapper = document.querySelector('.profile__tabs');
+const tabs = document.querySelectorAll('.tabs-item');
+const tabsWrapper = document.querySelector('.tabs');
 
 tabs.forEach((tab, index) => {
   tab.addEventListener('click', () => {
-    // Убираем активный класс у всех табов
-    tabs.forEach((t) => t.classList.remove('profile__tabs-item--active'));
-
-    // Добавляем активный класс текущему табу
-    tab.classList.add('profile__tabs-item--active');
-
-    // Передвигаем бордер к активному табу
+    tabs.forEach((t) => t.classList.remove('tabs-item--active'));
+    tab.classList.add('tabs-item--active');
     tabsWrapper.style.setProperty('--active-tab-index', index);
   });
 });
+
+
+
+// Main page Timer
+const hoursElement = document.querySelector('.highlighted__auction-hours');
+const minutesElement = document.querySelector('.highlighted__auction-minutes');
+const secondsElement = document.querySelector('.highlighted__auction-seconds');
+
+let hours = 11;
+let minutes = 32;
+let seconds = 7;
+
+function updateTimer() {
+    seconds--;
+
+    if (seconds < 0) {
+        seconds = 59;
+        minutes--;
+    }
+
+    if (minutes < 0) {
+        minutes = 59;
+        hours--;
+    }
+
+    if (hours < 0) {
+        clearInterval(timer); 
+        return;
+    }
+
+    hoursElement.textContent = String(hours).padStart(2, '0');
+    minutesElement.textContent = String(minutes).padStart(2, '0');
+    secondsElement.textContent = String(seconds).padStart(2, '0');
+}
+const timer = setInterval(updateTimer, 1000);
+
 
 window.addEventListener('resize', toggleFormModifier);
 window.addEventListener('load', toggleFormModifier);
